7c822bc12e088ec50f5e5d9d189dc0ab3da580f9
jdmevo123 <dale_lenard@hotmail.com>
2020-06-01 12:17:44 +1000

    all

--- akamai.py
+++ akamai.py
@@ -4,20 +4,20 @@ from akamai.edgegrid import EdgeGridAuth, EdgeRc
 #Setup API Client
 edge_session = requests.Session()
 edge_session.auth = EdgeGridAuth(
-    client_token=client_token,
-    client_secret=client_secret,
-    access_token=access_token
+    client_token=os.getenv('INPUT_CLIENT_TOKEN'),
+    client_secret=os.getenv('INPUT_CLIENT_SECRET'),
+    access_token=os.getenv('INPUT_ACCESS_TOKEN')
 )
-api_baseurl = 'https://%s' % host
+api_baseurl = 'https://%s' % os.getenv('INPUT_HOST')
 api_headers = {
     'Content-Type': 'application/json',
     'Accept': 'application / vnd.akamai.cps.enrollments.v4 + json'
 }
 
 #Make Request
-uri = '/ccu/v3/delete/cpcode/%s' % network
+uri = '/ccu/v3/delete/cpcode/%s' % os.getenv('INPUT_NETWORK')
 line_items = []
-line_items.append(cp_code)
+line_items.append(os.getenv('INPUT_CP_CODE'))
 result = edge_session.post(
         urljoin(api_baseurl, '%s' % uri),
         headers={'Content-Type': 'application/json'}, data = json.dumps(line_items))
